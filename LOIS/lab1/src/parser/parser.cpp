fc7822558ac011c9269f7a100a91478d11fc5a5f
Alex Yevik <aleksejevik92@gmail.com>
2026-04-04 22:44:41 +0300

    lois: feat: parser 1st version(not finished)

---
+++ LOIS/lab1/src/parser/parser.cpp
@@ -0,0 +1,51 @@
+﻿//
+// Created by Алексей on 04.04.2026.
+//
+#include "parser.h"
+#include <memory>
+
+std::shared_ptr<Node> Parser::parse(const std::string &input) {
+    src = input;
+    pos = 0;
+    variables.clear();
+    skip_spaces();
+    auto root = parse_formula();
+    skip_spaces();
+    if (pos != (int)src.size()) {
+        throw std::invalid_argument(
+            "Лишние символы после формулы на позиции "
+            + std::to_string(pos + 1)
+            + ": '" + src.substr(pos) + "'");
+    }
+    return root;
+}
+
+void Parser::skip_spaces() {
+    while (pos < (int)src.size() && src[pos] == ' ') {++pos;}
+}
+
+
+char Parser::peek(){
+    return (pos < (int)src.size()) ? src[pos] : '\0';
+}
+
+char Parser::consume() {
+    if (pos >= (int)src.size()) {
+        throw std::invalid_argument("Неожиданный конец строки");
+    }
+    return src[pos++];
+}
+
+void Parser::expect(char c) {
+    skip_spaces();
+    if (peek() != c) {
+        throw std::invalid_argument(
+                std::string("Ожидался '") + c + "', получен '"
+                + (peek() ? std::string(1, peek()) : "конец строки")
+                + "' на позиции " + std::to_string(pos + 1));
+    }
+    consume();
+}
+
+
+
---
+++ LOIS/lab1/src/parser/parser.h
@@ -0,0 +1,25 @@
+﻿//
+// Created by Алексей on 04.04.2026.
+//
+
+#ifndef LAB1_PARSER_H
+#define LAB1_PARSER_H
+#include <set>
+#include <string>
+#include <memory>
+#include <node/node.h>
+class Parser {
+public:
+    std::set<std::string> variables;
+    std::shared_ptr<Node> parse(const std::string& input);
+private:
+    std::string src;
+    int pos = 0;
+    void skip_spaces();
+    char peek();
+    char consume();
+    void expect(char c);
+    std::shared_ptr<Node> parse_formula();
+
+};
+#endif //LAB1_PARSER_H
