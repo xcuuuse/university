96b64ebecd1c42ed57898b8f969a3e25c2481e54
Alex Yevik <aleksejevik92@gmail.com>
2026-06-09 23:33:32 +0300

    repo

--- sem_2/oaip_tasks/35.cpp
+++
@@ -1,7 +0,0 @@
-int maxi(int n){
-    if (n == 0)
-    {
-        return 0;
-    }
-    return max(n % 10, maxi(n /  10));
-}
--- sem_2/oaip_tasks/39.cpp
+++
@@ -1,7 +0,0 @@
-int find_min(int* arr, int index, int size){
-    if (index == size - 1)
-    {
-        return arr[index];
-    }
-    return min(arr[index], find_min(arr, index + 1, size));
-}
